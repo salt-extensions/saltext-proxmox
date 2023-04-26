43ee44a688f2d95d6de7fc5602ca47be9c4b158c
Maarten Thibaut <mthibaut858@gmail.com>
2023-04-26 16:10:22 +0000

    initial migration from saltstack/salt

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+The changelog format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
+
+This project uses [Semantic Versioning](https://semver.org/) - MAJOR.MINOR.PATCH
+
+# Changelog
+
+Saltext.Proxmox 1.0.0 (2023-04-26)
+==================================
+
+Added
+-----
+
+- Initial version of Proxmox Cloud Modules Extension for Salt. This release
+  tracks the functionality in the core Salt code base as of version 3006.0.
---
+++ setup.py
@@ -0,0 +1,19 @@
+# pylint: disable=missing-module-docstring
+from pathlib import Path
+
+import setuptools
+
+version_path = Path(__file__).parent / "src" / "saltext" / "azurerm" / "version.py"
+
+with version_path.open() as f:
+    for line in f:
+        if line.startswith("__version__"):
+            # We only want the bare string pls
+            version = line.partition("=")[-1].strip().strip('"').strip("'")
+            break
+    else:
+        version = "0.0.1dev1"  # pylint: disable=invalid-name
+
+
+if __name__ == "__main__":
+    setuptools.setup(use_scm_version=True, version=version)
